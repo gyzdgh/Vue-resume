6e2c70df356f4cd02b447d20d1b7d9c63b797d11
高远 <gyz@192.168.2.108>
2018-07-26 20:07:47 +0800

    add contourStyle

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import "normalize.css/normalize.css";
+import "./assets/reset.css";
+
+import Topbar from "./components/Topbar";
+import ResumeEditor from "./components/ResumeEditor";
+import ResumePreview from "./components/ResumePreview";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function() {
+    return {
+      text: "你好"
+    };
+  },
+  components: { Topbar, ResumeEditor, ResumePreview }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #eaebec;
+}
+.page > main {
+  flex-grow: 1;
+}
+
+.page > main {
+  min-width: 1024px;
+  max-width: 1440px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 16px;
+  align-self: center;
+}
+
+#resumeEditor {
+  width: 35%;
+  background: #444;
+}
+#resumePreview {
+  flex-grow: 1;
+  margin-left: 16px;
+  background: #777;
 }
 </style>
