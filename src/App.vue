3ce1d70c15071b2999cc03e7496ee4a02ae0b5b5
高远 <gyz@192.168.2.108>
2018-07-27 18:04:31 +0800

    npm install sass-loader node-sass

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .page {
   height: 100vh;
   display: flex;
--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -16,3 +16,5 @@ export default {
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
 }
 </style>
+
+
