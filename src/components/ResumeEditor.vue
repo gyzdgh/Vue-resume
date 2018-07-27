bcab5349b02e15dc02bc160f9a2ea605b39726ca
高远 <gyz@192.168.2.108>
2018-07-27 19:08:39 +0800

    添加导航界面

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,19 +1,86 @@
 <template>
   <div id="resumeEditor">
-    I am resumeEditor
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
+          <svg class="icon">
+            <use :xlink:href="`#icon-${item.icon}`"></use>
+          </svg>
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.config" v-show="item.field === selected">
+        {{resume[item.field]}}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ResumeEditor"
+  name: "ResumeEditor",
+  data() {
+    return {
+      selected: "profile",
+      resume: {
+        config: [
+          { field: "profile", icon: "id" },
+          { field: "work history", icon: "work" },
+          { field: "education", icon: "book" },
+          { field: "projects", icon: "heart" },
+          { field: "awards", icon: "cup" },
+          { field: "contacts", icon: "phone" }
+        ],
+        profile: {
+          name: "",
+          city: "",
+          title: ""
+        },
+        "work history": [1],
+        education: [2],
+        projects: [3],
+        awards: [4],
+        contacts: [5]
+      }
+    };
+  }
 };
 </script>
 
-<style>
+<style lang="scss">
 #resumeEditor {
   background: #ffffff;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+  > nav {
+    width: 80px;
+    background: black;
+    color: white;
+    > ol {
+      > li {
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        &.active {
+          background: white;
+          color: black;
+        }
+      }
+    }
+  }
+  svg.icon {
+    width: 24px;
+    height: 24px;
+  }
+}
+ol {
+  list-style: none;
 }
 </style>
 
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
   <div id="topbar">
     <div class="wrapper">
       <span class="logo">Resumer</span>
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button class="primary2">预览</button>
+      </div>
     </div>
   </div>
 </template>
@@ -12,25 +16,47 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 #topbar {
   background: #ffffff;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  > .wrapper {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0 auto;
+    height: 64px;
+  }
+  > .wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 16px;
+  }
+  .logo {
+    font-size: 24px;
+    color: #000000;
+  }
 }
-#topbar > .wrapper {
-  min-width: 1024px;
-  max-width: 1440px;
-  margin: 0 auto;
-  height: 64px;
-}
-#topbar > .wrapper {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 16px;
-}
-#topbar .logo {
-  font-size: 24px;
-  color: #000000;
+button {
+  width: 72px;
+  height: 32px;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  background: #ddd;
+  color: #222;
+  &:hover {
+    box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.5);
+  }
+  &.primary {
+    background: #02af5f;
+    color: white;
+    outline-style: none;
+  }
+  &.primary2{
+    background: #ddd;
+    color: #222;
+    outline-style: none;
+  }
 }
 </style>
